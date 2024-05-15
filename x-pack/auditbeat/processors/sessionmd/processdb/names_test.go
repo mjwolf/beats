0741ee8708c8356e2bee59fd89c6bb1f9fe66c0a
Michael Wolf <michael.wolf@elastic.co>
2024-05-15 15:25:12 -0700

    add file read benchmark

--- x-pack/auditbeat/processors/sessionmd/processdb/names_test.go
+++ x-pack/auditbeat/processors/sessionmd/processdb/names_test.go
@@ -1,8 +1,11 @@
 package processdb
 
 import (
+	"os"
 	"testing"
 )
+
+const filename = "/etc/passwd"
 
 
 var username string
@@ -23,5 +26,14 @@ func BenchmarkUnCached(b *testing.B) {
 	u, f := c.getUserNameUncached("1000")
 	username = u
 	found = f
+  }
+}
+
+func BenchmarkFileRead(b *testing.B) {
+  for i := 0; i < b.N; i++ {
+	_, err := os.ReadFile(filename)
+    if err != nil {
+        b.Fatal(err)
+    }
   }
 }
